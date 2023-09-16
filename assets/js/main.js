791190133d06fdb5b061551986eca0d94f6aed28
Tala <70854429+Tala-cmd@users.noreply.github.com>
2023-09-16 15:10:37 +0300

    Scroll Bar & Scroll Up Button

--- assets/js/main.js
+++ assets/js/main.js
@@ -56,13 +56,17 @@ const sendEmail = (e) =>{
     //Show error message
     contactMessage.textContent= 'Message sending failed (Service error) ✘'
     }
-    
-    
     )
-
     //Clear input fields
     contactForm.reset()
 }
 
+contactForm.addEventListener('submit', sendEmail);
 
-contactForm.addEventListener('submit', sendEmail)
+/* === SHOW SCROLL UP === */
+const scrollUp = () =>{
+    const scrollUp= document.getElementById('scroll-up')
+    //When the scroll is higher than 350vh, add the class.
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
