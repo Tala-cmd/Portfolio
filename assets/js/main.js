29377539d1ef599fdc1c175d3a57dc5df3504f31
Tala <70854429+Tala-cmd@users.noreply.github.com>
2023-09-24 09:20:07 +0300

    Project section -1

--- assets/js/main.js
+++ assets/js/main.js
@@ -161,7 +161,7 @@ if (savedImage) {
 }
 
 
-/* === SCROLL REVEAL ANIMATION === */
+/* === SCROLL REVEAL ANIMATION === 
 const sr= ScrollReveal({
     origin:'top', 
     distance: '60px',
@@ -174,4 +174,4 @@ sr.reveal(`.home__profile, .about__image, .contact__mail`, {origin: 'right'})
 sr.reveal(`.home__name, .home__info, .about__container 
             .section__title-1, .about__info, .contact__social, .contact__data` 
             , {origin: 'left'})
-sr.reveal(`.services__card, .projects__card`, {interval: 100})
+sr.reveal(`.services__card, .projects__card`, {interval: 100})*/
